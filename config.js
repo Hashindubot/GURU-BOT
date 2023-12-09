6c4c69758ac0e38c996095c151e9a025a1d492f8
Hashindubot <153391260+Hashindubot@users.noreply.github.com>
2023-12-10 01:27:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94771163079', 'Hashindu', true],
+  ['94761988068', 'Hashindu', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.botname = 'ᴛʜᴇ 𝗛𝗔𝗦𝗛𝗜𝗡𝗗𝗨-𝗕𝗢𝗧'
+global.premium = 'False'
+global.packname = '𝗛𝗔𝗦𝗛𝗜𝗡𝗗𝗨┃ᴮᴼᵀ' 
+global.author = '@𝗛𝗔𝗦𝗛𝗜𝗡𝗗𝗨' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
